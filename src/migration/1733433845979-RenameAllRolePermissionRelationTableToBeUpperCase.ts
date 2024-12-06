54972304b1532e2279a5db98a912b36209703074
Huỳnh Đình Thịnh <118111790+HdThinh6102git@users.noreply.github.com>
2024-12-06 10:38:32 +0700

    fix: migration

--- src/migration/1733433845979-RenameAllRolePermissionRelationTableToBeUpperCase.ts
+++ src/migration/1733433845979-RenameAllRolePermissionRelationTableToBeUpperCase.ts
@@ -1,24 +1,24 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+// import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class RenameAllRolePermissionRelationTableToBeUpperCase1733433845979 implements MigrationInterface {
+// export class RenameAllRolePermissionRelationTableToBeUpperCase1733433845979 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        // Rename the table to uppercase
-        await queryRunner.renameTable('role_permissions_permission', 'ROLE_PERMISSIONS_PERMISSION');
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         // Rename the table to uppercase
+//         await queryRunner.renameTable('role_permissions_permission', 'ROLE_PERMISSIONS_PERMISSION');
 
-        // Rename columns to uppercase
-        await queryRunner.renameColumn('ROLE_PERMISSIONS_PERMISSION', 'rOLEID', 'ROLEID');
-        await queryRunner.renameColumn('ROLE_PERMISSIONS_PERMISSION', 'pERMISSIONID', 'PERMISSIONID');
-    }
+//         // Rename columns to uppercase
+//         await queryRunner.renameColumn('ROLE_PERMISSIONS_PERMISSION', 'rOLEID', 'ROLEID');
+//         await queryRunner.renameColumn('ROLE_PERMISSIONS_PERMISSION', 'pERMISSIONID', 'PERMISSIONID');
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        // Revert the table name to lowercase
-        await queryRunner.renameTable('ROLE_PERMISSIONS_PERMISSION', 'role_permissions_permission');
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         // Revert the table name to lowercase
+//         await queryRunner.renameTable('ROLE_PERMISSIONS_PERMISSION', 'role_permissions_permission');
 
-        // Revert columns to lowercase
-        await queryRunner.renameColumn('role_permissions_permission', 'ROLEID', 'roleid');
-        await queryRunner.renameColumn('role_permissions_permission', 'PERMISSIONID', 'permissionid');
+//         // Revert columns to lowercase
+//         await queryRunner.renameColumn('role_permissions_permission', 'ROLEID', 'roleid');
+//         await queryRunner.renameColumn('role_permissions_permission', 'PERMISSIONID', 'permissionid');
         
-    }
+//     }
 
-}
+// }
